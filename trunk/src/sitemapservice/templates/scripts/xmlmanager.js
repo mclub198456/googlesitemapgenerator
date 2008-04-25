5c212e12807be829ba50c9e8bf7970b890fb0f73
chaiying@google.com <chaiying@google.com@201b6931-4c48-0410-a82a-592b292e4125>
2008-04-25 02:25:06 +0000

    replace the license in the JS

--- trunk/src/sitemapservice/templates/scripts/xmlmanager.js
+++ trunk/src/sitemapservice/templates/scripts/xmlmanager.js
@@ -1,5 +1,26 @@
-// Copyright 2007 Google Inc.
-// All Rights Reserved.
+// Copyright 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// This is the top level setting class. It contains all the setting values for
+// this application. Besides site specific settings, this class also includes
+// application level configuration, like back-up duration, remote admin port,
+// admin account, and etc. Especially, there is global setting field, which
+// contains default values for site settings. Please see the member fields
+// doc for details.
+// Besides the xml setting load/save/validate functions, it provides functions
+// to load values from file, as well as save value to a file.
+// This class is not thread-safe.
+
 
 /**
  * @fileoverview This file contains Class XmlManager that extends the XML node
